a8b2e1585b30d0d848e3187ddc15978c71176493
Daniel Abdulahad <daniel00.abdulahad@gmail.com>
2022-04-19 18:38:00 +0200

    Added CLI commands

---
+++ src/EzLiveServer.CLI/FileServerCliParameters.cs
@@ -0,0 +1,11 @@
+﻿using Cocona;
+
+internal class FileServerCliParameters : ICommandParameterSet
+{
+    [Option('d', Description = "Specifies the root directory to serve static files from.")]
+    public string Directory { get; set; } = default!;
+
+    [Option('p', Description = "Port to listen to.")]
+    [HasDefaultValue]
+    public int Port { get; set; } = 5069;
+}
--- src/EzLiveServer.CLI/Program.cs
+++ src/EzLiveServer.CLI/Program.cs
@@ -1,21 +1,25 @@
-﻿using EzLiveServer.Core;
+﻿using Cocona;
+using EzLiveServer.Core;
 using EzLiveServer.Core.Options;
 
-var options = new FileServerOptionsBuilder()
-    .WithPort(5069)
-    .WithBaseDirectory(@"/")
-    .WithInjectedFilePath("./StaticContent/InjectedToResponse.html")
-    .WithDefaultNotFoundFilePath("./StaticContent/Default404NotFound.html")
-    .Build();
-
-using var server = new FileServer(options);
-
-Console.WriteLine("Starting server...");
-server.Start();
-
-foreach (string uri in server.UriPrefixes)
+await CoconaLiteApp.RunAsync(async (FileServerCliParameters cliParameters) =>
 {
-    Console.WriteLine($"Listening on: {uri}");
-}
+    var options = new FileServerOptionsBuilder()
+        .WithInjectedFilePath("./StaticContent/InjectedToResponse.html")
+        .WithDefaultNotFoundFilePath("./StaticContent/Default404NotFound.html")
+        .WithBaseDirectory(cliParameters.Directory)
+        .WithPort(cliParameters.Port)
+        .Build();
 
-await server.StartListenToRequestsAsync();
+    using var server = new FileServer(options);
+
+    Console.WriteLine("Starting server...");
+    server.Start();
+
+    foreach (string uri in server.UriPrefixes)
+    {
+        Console.WriteLine($"Listening on: {uri}");
+    }
+
+    await server.StartListenToRequestsAsync();
+});
